d5e81af7c3aeb228be036d9af3b10ea8bfa495d7
Travis Johnson <travisj7@gmail.com>
2016-09-04 19:16:41 +1000

    Initial

---
+++ src/components/course/ManageCoursePage.js
@@ -0,0 +1,39 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
+
+class ManageCoursePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
+  render() {
+    const {courses} = this.props;
+    return (
+      <h1>Manage Course</h1>
+    );
+  }
+}
+
+ManageCoursePage.propTypes = {
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
